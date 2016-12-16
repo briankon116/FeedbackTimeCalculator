103859cd21926bec8627214cede0e3cba0267617
Brian Kondracki <brian.kondracki@stonybrook.edu>
2016-12-15 20:34:52 -0500

    Added a count for the CSL to see what percentage of the cases out of the given files were resolved.

--- FeedbackTimeCalculator.py
+++ FeedbackTimeCalculator.py
@@ -5,6 +5,8 @@ import xlrd, os
 totalUnanswered = 0 
 totalResponseTime = 0.0
 totalResponses = 0
+totalCSLCompletes = 0
+totalCSLCases = 0
 
 def loopThroughFiles():
     global totalUnanswered
@@ -17,38 +19,80 @@ def loopThroughFiles():
         
         # Open the workbook
         workbook = xlrd.open_workbook("files/" + file)
-
-        # Open the first sheet
-        worksheet = workbook.sheet_by_index(0)
         
+        if('mydtxt' in file.lower()):
+            calculateDeezFeedbackTime(workbook)
+            continue
+        elif('csl' in file.lower()):
+            calculateCSLCompleteness(workbook)
+            continue
+        
+    
+    totalAverageResponseTime = totalResponseTime / totalResponses
+    cslCompleteRate = float(totalCSLCompletes) / float(totalCSLCases) * 100
+    
+    print("Average Response Time: %.3f hours" % totalAverageResponseTime)
+    print("Total unanswered: %d" % totalUnanswered)
+    print("CSL Complete Rate: %.2f%%" % cslCompleteRate)
+
+    
+def calculateCSLCompleteness(workbook):
+    global totalCSLCompletes
+    global totalCSLCases
+    
+    # Loop over all of the sheets in the workbook
+    for i in range (0, len(workbook.sheet_names())):
+        # Open the current sheet
+        worksheet = workbook.sheet_by_index(i)
+
         # The starting row since the title rows mean nothing
         row = 2
         
         while(row < worksheet.nrows):
-            receivedTime = worksheet.cell(row, 0).value
-            
-            if(receivedTime == ""):
-                row += 1
-                continue
-
-            responseSent = worksheet.cell(row, 2).value
-            
-            if(responseSent == ""):
-                totalUnanswered += 1
+            if(worksheet.cell(row, 0).value == ""):
                 row += 1
                 continue
             
-            responseTime = calculateDifference(receivedTime, responseSent)
-            
-            if(responseTime != -1):
-                totalResponseTime += calculateDifference(receivedTime, responseSent)
-                totalResponses += 1
+            if(worksheet.cell(row, 8).value.lower() == 'y'):
+                totalCSLCompletes += 1
+            totalCSLCases += 1
             row += 1
+        
     
-    totalAverageResponseTime = totalResponseTime / totalResponses
+def calculateDeezFeedbackTime(workbook):
+    global totalUnanswered
+    global totalResponseTime
+    global totalResponses
     
-    print("Average Response Time: %.3f hours" % totalAverageResponseTime)
 
+    # Open the first sheet
+    worksheet = workbook.sheet_by_index(0)
+
+    # The starting row since the title rows mean nothing
+    row = 2
+
+    while(row < worksheet.nrows):
+        receivedTime = worksheet.cell(row, 0).value
+
+        if(receivedTime == ""):
+            row += 1
+            continue
+
+        responseSent = worksheet.cell(row, 2).value
+
+        if(responseSent == ""):
+            totalUnanswered += 1
+            row += 1
+            continue
+
+        responseTime = calculateDifference(receivedTime, responseSent)
+
+        if(responseTime != -1):
+            totalResponseTime += calculateDifference(receivedTime, responseSent)
+            totalResponses += 1
+        row += 1
+    
+    
 def calculateDifference(time1, time2):
     try:
         date=dt.datetime.strptime(time1,'%m/%d/%Y %I:%M:%S %p')
